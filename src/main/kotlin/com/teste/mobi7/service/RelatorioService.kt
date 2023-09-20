18789e1e695a7cd6d5a338b8b3c3f6f7c2bae81d
Luan Feo <luanfeo@gmail.com>
2023-09-20 10:51:45 -0300

    esboco da logica de tempo

--- src/main/kotlin/com/teste/mobi7/service/RelatorioService.kt
+++ src/main/kotlin/com/teste/mobi7/service/RelatorioService.kt
@@ -4,6 +4,8 @@ import com.teste.mobi7.model.PontoDeInteresse
 import com.teste.mobi7.model.PosicaoVeiculo
 import com.teste.mobi7.repository.PontoDeInteresseRepository
 import com.teste.mobi7.repository.PosicaoVeiculoRepository
+import org.apache.commons.collections4.SetValuedMap
+import org.apache.commons.collections4.multimap.HashSetValuedHashMap
 import org.locationtech.jts.geom.Coordinate
 import org.locationtech.jts.geom.Geometry
 import org.locationtech.jts.geom.GeometryFactory
@@ -19,141 +21,194 @@ import kotlin.math.cos
 
 @Service
 class RelatorioService(
-    val posicaoVeiculoRepository: PosicaoVeiculoRepository,
-    val pontoDeInteresseRepository: PontoDeInteresseRepository,
+	val posicaoVeiculoRepository: PosicaoVeiculoRepository,
+	val pontoDeInteresseRepository: PontoDeInteresseRepository,
 ) {
 
-    // todo - depois trocar para as services
+	// todo - depois trocar para as services
 
 
-    fun buscarPorFiltro() {
+	fun buscarPorFiltro() {
 
-        // criar map da relacao placaCarro X (local e tempo)
+		// criar map da relacao placaCarro X (local e tempo)
 
+		var pontoDeInteresseLista = generateList2()
+		var posicaoVeiculoLista = generateList()
 
-        var ponto = PontoDeInteresse(1, "padaria do ze", 100, -25.36496636999715, -51.46980205405271)
-        var posicaoVeiculoLista = generateList()
-        posicaoVeiculoLista.sortBy { it.dataPosicao }
-
-        for (posicaoVeiculo in posicaoVeiculoLista) {
-            // pegar o ponto anterior
-            //nao fazer essa logica no primeiro ponto apenas se tiver apenas um ponto na lista
-
-            if (estaDentroDoCirculo(converterEmCirculo(ponto), converterEmPonto(posicaoVeiculo))) {
-                calcularTempoEntreDuasDatasEmMinutos(LocalDateTime.now(), posicaoVeiculo.dataPosicao)
-            }
-
-        }
-        // val listaPosicoes = posicaoVeiculoRepository.findAll()
-        // val listaPoi = pontoDeInteresseRepository.findAll()
+		val mapPlacaVeiculoPosicaoVeiculo = separarPorPlacaDeVeiculo(posicaoVeiculoLista)
 
 
-        // buscar as posicoes por data e/ou placa de veiculo
-
-        /*
-            busca as posicoes
-            separa por veiculo ou separa por poi
-            faz o calculo do poi
-            retorna a lista de poi por veiculo
-         */
-    }
-
-    private fun calcularTempoEntreDuasDatasEmMinutos(
-        dataPontoAtual: LocalDateTime,
-        dataPontoAnterior: LocalDateTime
-    ): Long {
-        return ChronoUnit.MINUTES.between(dataPontoAtual, dataPontoAnterior)
-    }
-
-    private fun estaDentroDoCirculo(
-        circulo: Polygon,
-        ponto: Geometry
-    ): Boolean {
-        return circulo.contains(ponto)
-    }
-
-    private fun converterEmPonto(latitude: Double, longitude: Double): Geometry {
-        val geometryFactory = GeometryFactory()
-        return geometryFactory.createPoint(Coordinate(latitude, longitude))
-    }
-
-    private fun converterEmPonto(posicaoVeiculo: PosicaoVeiculo): Geometry {
-        return converterEmPonto(posicaoVeiculo.latitude, posicaoVeiculo.longitude)
-    }
-
-    private fun converterEmCirculo(pontoDeInteresse: PontoDeInteresse): Polygon {
-        return converterEmCirculo(
-            pontoDeInteresse.latitude,
-            pontoDeInteresse.longitude,
-            pontoDeInteresse.raioEmMetros.toDouble()
-        )
-    }
+		percorrerMap(mapPlacaVeiculoPosicaoVeiculo, pontoDeInteresseLista)
 
 
-    private fun converterEmCirculo(latitude: Double, longitude: Double, raioEmMetros: Double): Polygon {
-        // -25.36496636999715, -51.46980205405271
-        val shapeFactory = GeometricShapeFactory()
-        shapeFactory.setNumPoints(64)
-
-        shapeFactory.setCentre(Coordinate(latitude, longitude))
-        // Length in meters of 1° of latitude = always 111.32 km
-        // Length in meters of 1° of latitude = always 111.32 km
-        shapeFactory.setWidth(raioEmMetros / 111320.0)
-        // Length in meters of 1° of longitude = 40075 km * cos( latitude ) / 360
-        // Length in meters of 1° of longitude = 40075 km * cos( latitude ) / 360
-        shapeFactory.setHeight(raioEmMetros / (40075000 * cos(Math.toRadians(latitude)) / 360))
-
-        return shapeFactory.createEllipse()
-    }
+//		for (posicaoVeiculo in posicaoVeiculoLista) {
+//			// pegar o ponto anterior
+//			//nao fazer essa logica no primeiro ponto apenas se tiver apenas um ponto na lista
+//
+//			if (estaDentroDoCirculo(converterEmCirculo(ponto), converterEmPonto(posicaoVeiculo))) {
+//				calcularTempoEntreDuasDatasEmMinutos(LocalDateTime.now(), posicaoVeiculo.dataPosicao)
+//			}
+//
+//		}
+		// val listaPosicoes = posicaoVeiculoRepository.findAll()
+		// val listaPoi = pontoDeInteresseRepository.findAll()
 
 
-    private fun generateList(): MutableList<PosicaoVeiculo> {
-        var list = mutableListOf<PosicaoVeiculo>()
+		// buscar as posicoes por data e/ou placa de veiculo
 
-        list.add(
-            PosicaoVeiculo(
-                2,
-                "teste",
-                LocalDateTime.of(LocalDate.now(), LocalTime.of(11, 20)),
-                0,
-                -25.364617,
-                -51.469893,
-                false
-            )
-        ) // dentro
-        list.add(
-            PosicaoVeiculo(
-                3,
-                "teste",
-                LocalDateTime.of(LocalDate.now(), LocalTime.of(10, 30)),
-                0,
-                -25.364966,
-                -51.479802,
-                false
-            )
-        ) // fora
-        list.add(
-            PosicaoVeiculo(
-                4,
-                "teste",
-                LocalDateTime.of(LocalDate.now(), LocalTime.of(12, 40)),
-                0,
-                -25.366574,
-                -51.4716927,
-                false
-            )
-        ) // fora
-        list.add(
-            PosicaoVeiculo(
-                5,
-                "teste",
-                LocalDateTime.of(LocalDate.now(), LocalTime.of(10, 50)),
-                0,
-                -25.3651379,
-                -51.4697745,
-                false
-            )
-        ) // dentro
-        return list
-    }
+		/*
+			busca as posicoes
+			separa por veiculo ou separa por poi
+			faz o calculo do poi
+			retorna a lista de poi por veiculo
+		 */
+	}
+
+	private fun percorrerMap(
+		mapPlacaVeiculoPosicaoVeiculo: SetValuedMap<String, PosicaoVeiculo>,
+		pontoInteresseLista: MutableList<PontoDeInteresse>
+	) {
+
+		for (placaVeiculoString in mapPlacaVeiculoPosicaoVeiculo.keySet()) {
+
+			var listPosicaoVeiculo = mapPlacaVeiculoPosicaoVeiculo.get(placaVeiculoString).toList()
+			listPosicaoVeiculo = listPosicaoVeiculo.sortedBy { it.dataPosicao }
+
+			var i = 0
+
+			for (posicaoVeiculo in listPosicaoVeiculo) {
+				if (listPosicaoVeiculo.size > 1 && i == 0) {
+					i += 1;
+					continue
+				} else if (i >= 1) {
+					pontoInteresseLista.forEach {
+						if (estaDentroDoCirculo(converterEmCirculo(it), converterEmPonto(posicaoVeiculo))) {
+							val posicaoAnterior = listPosicaoVeiculo.get(i - 1)
+							val tempoDentroDoPOIEmMinutos = calcularTempoEntreDuasDatasEmMinutos(
+								posicaoVeiculo.dataPosicao,
+								posicaoAnterior.dataPosicao
+							)
+						}
+					}
+				}
+				i += 1;
+			}
+		}
+	}
+
+
+	private fun separarPorPlacaDeVeiculo(posicaoVeiculoList: MutableList<PosicaoVeiculo>): SetValuedMap<String, PosicaoVeiculo> {
+
+		var map: SetValuedMap<String, PosicaoVeiculo> = HashSetValuedHashMap()
+
+		for (posicao in posicaoVeiculoList) {
+			map.put(posicao.placaVeiculo, posicao)
+		}
+
+		return map
+	}
+
+	private fun calcularTempoEntreDuasDatasEmMinutos(
+		dataPontoAtual: LocalDateTime,
+		dataPontoAnterior: LocalDateTime
+	): Long {
+		return ChronoUnit.MINUTES.between(dataPontoAtual, dataPontoAnterior)
+	}
+
+	private fun estaDentroDoCirculo(circulo: Polygon, ponto: Geometry): Boolean {
+		return circulo.contains(ponto)
+	}
+
+	private fun converterEmPonto(latitude: Double, longitude: Double): Geometry {
+		val geometryFactory = GeometryFactory()
+		return geometryFactory.createPoint(Coordinate(latitude, longitude))
+	}
+
+	private fun converterEmPonto(posicaoVeiculo: PosicaoVeiculo): Geometry {
+		return converterEmPonto(posicaoVeiculo.latitude, posicaoVeiculo.longitude)
+	}
+
+	private fun converterEmCirculo(pontoDeInteresse: PontoDeInteresse): Polygon {
+		return converterEmCirculo(
+			pontoDeInteresse.latitude,
+			pontoDeInteresse.longitude,
+			pontoDeInteresse.raioEmMetros.toDouble()
+		)
+	}
+
+	private fun converterEmCirculo(latitude: Double, longitude: Double, raioEmMetros: Double): Polygon {
+		// -25.36496636999715, -51.46980205405271
+		val shapeFactory = GeometricShapeFactory()
+		shapeFactory.setNumPoints(64)
+
+		shapeFactory.setCentre(Coordinate(latitude, longitude))
+		// Length in meters of 1° of latitude = always 111.32 km
+		// Length in meters of 1° of latitude = always 111.32 km
+		shapeFactory.setWidth(raioEmMetros / 111320.0)
+		// Length in meters of 1° of longitude = 40075 km * cos( latitude ) / 360
+		// Length in meters of 1° of longitude = 40075 km * cos( latitude ) / 360
+		shapeFactory.setHeight(raioEmMetros / (40075000 * cos(Math.toRadians(latitude)) / 360))
+
+		return shapeFactory.createEllipse()
+	}
+
+	private fun generateList(): MutableList<PosicaoVeiculo> {
+		var list = mutableListOf<PosicaoVeiculo>()
+
+		list.add(
+			PosicaoVeiculo(
+				2,
+				"teste",
+				LocalDateTime.of(LocalDate.now(), LocalTime.of(11, 20)),
+				0,
+				-25.364617,
+				-51.469893,
+				false
+			)
+		) // dentro
+		list.add(
+			PosicaoVeiculo(
+				3,
+				"teste",
+				LocalDateTime.of(LocalDate.now(), LocalTime.of(10, 30)),
+				0,
+				-25.364966,
+				-51.479802,
+				false
+			)
+		) // fora
+		list.add(
+			PosicaoVeiculo(
+				4,
+				"teste",
+				LocalDateTime.of(LocalDate.now(), LocalTime.of(12, 40)),
+				0,
+				-25.366574,
+				-51.4716927,
+				false
+			)
+		) // fora
+		list.add(
+			PosicaoVeiculo(
+				5,
+				"teste",
+				LocalDateTime.of(LocalDate.now(), LocalTime.of(10, 50)),
+				0,
+				-25.3651379,
+				-51.4697745,
+				false
+			)
+		) // dentro
+		return list
+	}
+
+	private fun generateList2(): MutableList<PontoDeInteresse> {
+		var list = mutableListOf<PontoDeInteresse>()
+
+		list.add(PontoDeInteresse(1, "padaria do ze", 100, -25.36496636999715, -51.46980205405271))
+		list.add(PontoDeInteresse(1, "padaria do ze", 100, -25.36496636999715, -51.46980205405271))
+		list.add(PontoDeInteresse(1, "padaria do ze", 100, -25.36496636999715, -51.46980205405271))
+
+		return list
+	}
 }
